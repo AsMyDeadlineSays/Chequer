614c846f08dc7c86a66077955a8f2d6f5625b90a
Святослав <kovalev.svyatoslav42@gmail.com>
2018-03-03 16:47:39 +0300

    structure

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const express = require('express')
+const path = require('path')
+
+
 const app = express()
 const root = __dirname + '/../'
-//app.engine('pug', require('pug').__express)
-app.set('views', __dirname + '/views/');
+const static_dir = path.join(root, 'dist', 'static')
+
+
+
+app.use(express.static('/static/', static_dir));
 
 app.get('*', function (req, res) {
   res.send_file(root + '/static/index.html');
-})
+});
 
-app.get('')
+//app.get('');
 
 app.listen(3000, () => console.log('App listening on port 3000'))
