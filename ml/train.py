c4999f56843a68770757aabd0529c96425f7d5ed
Yarmoon <jar.lev@yandex.ru>
2018-03-04 09:03:05 +0300

    Update train.py

--- ml/train.py
+++ ml/train.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
 import pandas as pd
 import pickle
+import os
 
 data = pd.read_csv('data.csv', delimiter='|')
 
@@ -12,5 +13,5 @@ clf = SGDClassifier()
 pipe = Pipeline([('vect', CountVectorizer()),('clf', clf)])
 pipe = pipe.fit(data['descr'], data['cat'])
 
-f = open('classifier.pk', 'wb')
+f = open(os.path.realpath(__file__)[:-8] + 'classifier.pk', 'wb')
 pickle.dump(pipe, f)
