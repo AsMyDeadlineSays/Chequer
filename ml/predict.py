a82f2620c4f63786d63c5bfed818afd111fd281a
Yarmoon <jar.lev@yandex.ru>
2018-03-04 09:01:26 +0300

    Update predict.py

--- ml/predict.py
+++ ml/predict.py
@@ -1,15 +1,18 @@
 import sys
 import pickle
+import os
+from json import dumps
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 
 args = sys.argv
 
-to_pred = args[1:]
+to_pred = ' '.join(args[1:])
 
-f = open('/home/svyatoslav/Foodhack/ml/classifier.pk', 'rb')
+to_pred = to_pred.split('|')
 
+f = open(os.path.realpath(__file__)[:-10] + 'classifier.pk', 'rb')
 clf = pickle.load(f)
 
-print(clf.predict(to_pred))
+print(*clf.predict(to_pred))
