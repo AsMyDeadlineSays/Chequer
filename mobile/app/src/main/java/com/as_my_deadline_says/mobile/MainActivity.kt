a90b4bea70a4d7f00d4eb182106f18450b7df8f2
Святослав <kovalev.svyatoslav42@gmail.com>
2018-03-06 00:04:16 +0300

    MOBILE: change some code

--- mobile/app/src/main/java/com/as_my_deadline_says/mobile/MainActivity.kt
+++ mobile/app/src/main/java/com/as_my_deadline_says/mobile/MainActivity.kt
@@ -3,6 +3,7 @@ package com.as_my_deadline_says.mobile
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,15 +11,18 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
+                input_to_buy.visibility = View.GONE
                 message.setText(R.string.title_share)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+                input_to_buy.visibility = View.VISIBLE
+                message.setText(R.string.title_to_buy)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+                input_to_buy.visibility = View.GONE
+                message.setText(R.string.title_scan)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -28,7 +32,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navigation.selectedItemId = R.id.navigation_dashboard
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.selectedItemId = R.id.navigation_dashboard
     }
 }
